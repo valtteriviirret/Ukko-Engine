8767906b797ab6de4195b3de0637cd50c8997c98
valtteriviirret <viirretvaltteri@gmail.com>
2022-01-14 21:32:00 +0200

    initial commit

--- src/main.cc
+++ src/main.cc
@@ -1,38 +1,55 @@
-#include "SDL.h"
+#include "SDL2/SDL.h"
+#include "Window.hh"
+#include "Renderer.hh"
 #include "globals.hh"
 
 #define FPS 60
 
-
-int main(int argc, char* argv[]) {
-
+int main(int argc, char* argv[]) 
+{
     const int frameDelay = 1000 / FPS;
 
     Uint32 frameStart;
     unsigned int frameTime;
+	Window window;
 
-    SDL_Window* window;
     SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
-    // Game loop:
-    while (!applicationShouldClose)
-    {
-        frameStart = SDL_GetTicks();
+	// event handler
+	SDL_Event e;
 
-        Update();
+	// Game loop:
+	while (!applicationShouldClose)
+	{
+		// handle events on queue
+		while(SDL_PollEvent(&e) != 0)
+		{
+			// user requests quit
+			if(e.type == SDL_QUIT)
+				applicationShouldClose = true;
+		}
 
-        frameTime = SDL_GetTicks() - frameStart;
+		// draw white screen
+		SDL_SetRenderDrawColor(Renderer::get(), 0xFF, 0xFF, 0xFF, 0xFF);
+		SDL_RenderClear(Renderer::get());
 
-        if(frameDelay > frameTime)
-            SDL_Delay(frameDelay - frameTime);
+		frameStart = SDL_GetTicks();
 
-    }
+		Update();
 
-    // Clean up
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+		frameTime = SDL_GetTicks() - frameStart;
+
+		if(frameDelay > frameTime)
+			SDL_Delay(frameDelay - frameTime);
+		
+		// update screen
+		SDL_RenderPresent(Renderer::get());
+
+	}
+
+	// close SDL
+	SDL_Quit();
+
     return 0;
 }
 
