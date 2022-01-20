50cbe45b7e1370862e6192a9ef0fb3987a4de0ee
valiant <antero.lindell@gmail.com>
2022-01-20 13:20:45 +0200

    fixed typo

--- src/SquareManager.cc
+++ src/SquareManager.cc
@@ -5,7 +5,7 @@ namespace Sqr
 	// the actual squares
 	Square squares[8][8];
 
-	// getting invidual square
+	// getting individual square
 	Square& getSquare(int x, int y) { return squares[x][y]; }
 
 	// handle mouse events in square
