fe7dedb199f555b74433f79f46960cf4c3210473
valtteriviirret <viirretvaltteri@gmail.com>
2022-01-20 20:36:13 +0200

    engine testing

--- src/Engine/Engine.cc
+++ src/Engine/Engine.cc
@@ -2,4 +2,14 @@
 
 namespace Engine
 {
+	void PlayMove()
+	{
+		if(Sqr::getSquare(0, 1).piece == ROOK)
+			std::cout << "Engine sees squares";
+		else
+			std::cout << "Engine is still stupid";
+
+		std::cout << (Sqr::getSquare(0, 1).piece);
+		std::cout << (Sqr::getSquare(0, 1).piece);
+	}
 }
--- src/Engine/Engine.hh
+++ src/Engine/Engine.hh
@@ -1,9 +1,14 @@
 #ifndef ENGINE_HH
 #define ENGINE_HH
 
+#include <iostream>
+#include "../SquareManager.hh"
+#include "../Square.hh"
+#include "../Piece.hh"
+
 namespace Engine
 {
-	class Initializer;
+	void PlayMove();
 };
 
 #endif
