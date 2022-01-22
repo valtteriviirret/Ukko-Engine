e7355270a455786c4181ddf128a4c63515d5291b
valtteriviirret <viirretvaltteri@gmail.com>
2022-01-22 23:55:02 +0200

    Horsey

--- src/LegalMoves.cc
+++ src/LegalMoves.cc
@@ -1,4 +1,22 @@
 #include "LegalMoves.hh"
+#include "SquareManager.hh"
+
+void KnightFunc(Piece piece, int x, int y, std::vector<Square> v)
+{
+	if(Sqr::getSquare(x, y).piece.type == NONE)
+		v.push_back(Sqr::getSquare(x, y));
+	else
+		if(Sqr::getSquare(x, y).piece.color != piece.color)
+			v.push_back(Sqr::getSquare(x, y));
+}
+
+/*
+void RookFunc(Piece piece, int dir, std::vector<Square>v)
+{
+
+}
+*/
+
 
 namespace LegalMove
 {
@@ -21,7 +39,8 @@ namespace LegalMove
 
 					// left
 					if(Sqr::getSquare((piece.x - 1), (piece.y - 1)).piece.type != NONE)
-						sqrs.push_back(Sqr::getSquare((piece.x - 1), (piece.y - 1)));
+						if(Sqr::getSquare((piece.x - 1), (piece.y - 1)).piece.color != piece.color)
+							sqrs.push_back(Sqr::getSquare((piece.x - 1), (piece.y - 1)));
 
 					// center
 					if(Sqr::getSquare(piece.x, (piece.y - 1)).piece.type == NONE)
@@ -29,7 +48,8 @@ namespace LegalMove
 
 					// right
 					if(Sqr::getSquare((piece.x + 1), (piece.y - 1)).piece.type != NONE)
-						sqrs.push_back(Sqr::getSquare((piece.x + 1), (piece.y - 1)));
+						if(Sqr::getSquare((piece.x + 1), (piece.y - 1)).piece.color != piece.color)
+							sqrs.push_back(Sqr::getSquare((piece.x + 1), (piece.y - 1)));
 
 				}
 
@@ -41,25 +61,52 @@ namespace LegalMove
 							sqrs.push_back(Sqr::getSquare(piece.x, 3));
 				
 					if(Sqr::getSquare((piece.x + 1), piece.y + 1).piece.type != NONE)
-						sqrs.push_back(Sqr::getSquare((piece.x + 1), (piece.y + 1)));
+						if(Sqr::getSquare((piece.x + 1), piece.y + 1).piece.color != piece.color)
+							sqrs.push_back(Sqr::getSquare((piece.x + 1), (piece.y + 1)));
  					
 					if(Sqr::getSquare(piece.x, (piece.y + 1)).piece.type == NONE)
 						sqrs.push_back(Sqr::getSquare(piece.x, (piece.y + 1)));
 
 					if(Sqr::getSquare((piece.x - 1), (piece.y + 1)).piece.type != NONE)
-						sqrs.push_back(Sqr::getSquare((piece.x - 1), (piece.y + 1)));
-
+						if(Sqr::getSquare((piece.x - 1), (piece.y + 1)).piece.color != piece.color)
+							sqrs.push_back(Sqr::getSquare((piece.x - 1), (piece.y + 1)));
 				}
-
 			break;
-			case KNIGHT: break;
-			case QUEEN: break;
+			case KNIGHT:
+				KnightFunc(piece, 2, 1, sqrs);
+				KnightFunc(piece, 1, 2, sqrs);
+				KnightFunc(piece, 2, -1, sqrs);
+				KnightFunc(piece, -2, 1, sqrs);
+				KnightFunc(piece, -1, 2, sqrs);
+				KnightFunc(piece, -2, 1, sqrs);
+				KnightFunc(piece, -2, -1, sqrs);
+				KnightFunc(piece, -2, -1, sqrs);
+			break;
+			case QUEEN: 
+					
+				
+			break;
 			case KING: break;
 			case BISHOP: break;
-			case ROOK: break;
+
+			case ROOK:
+			/*
+				for(int i = piece.x; ;i++)
+				{
+					if(Sqr::getSquare(i, piece.y).piece.type == NONE)
+						sqrs.push_back(Sqr::getSquare(i, piece.y));
+					else
+						if(Sqr::getSquare(i, piece.y).piece.color != piece.color)
+							sqrs.push_back(Sqr::getSquare(i, piece.y));
+					break;
+				}
+			*/
+			break;
 			case NONE: break;
 		}
 
 		return sqrs;
 	}
 }
+
+
